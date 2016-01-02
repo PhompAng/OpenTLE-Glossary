d7ac5c223fc35be65ce7e63a35b32d20dcf6c089
Pichai Sivawat <tingtong003tomy@gmail.com>
2016-01-03 03:01:00 +0700

    optimize filter

--- js/result.js
+++ js/result.js
@@ -7,13 +7,12 @@ export default class Result extends React.Component {
         super(props)
     }
 
+    wordFilter(w) {
+        return w.word.substring(0, this.length).indexOf(this) !== -1
+    }
+
     render() {
-        let rows = []
-        for (let index in glossary) {
-            if (glossary[index].word.substring(0, this.props.input.length).indexOf(this.props.input) !== -1) {
-                rows.push(<Row key={index} word={glossary[index].word} meaning={glossary[index].meaning} />)
-            }
-        }
+        let rows = glossary.filter(this.wordFilter, this.props.input).map(w => <Row key={w.key} word={w.word} meaning={w.meaning} />)
         return <table className="highlight">
             <thead>
               <tr>
